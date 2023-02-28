b5cc8eb7c04af8fb797c3abf0d6a781a1cbb89b0
xx-Flex-xx <endlessxxxpain@gmail.com>
2023-02-28 16:13:24 +0300

    ADD WAL

--- src/middlewares/validatorVideos.ts
+++ src/middlewares/validatorVideos.ts
@@ -3,7 +3,6 @@ import {  body,  validationResult } from 'express-validator';
 
 export const videosValidator =
     [
-
         body('title').trim().isLength({ min: 0, max: 40}).isString || null,
         body('author').isString().isLength({max: 20}),
         body('availableResolutions').isString().isEmpty(),
