711142550601be9197ed16fcda0ecf34c76cf405
xx-Flex-xx <endlessxxxpain@gmail.com>
2023-02-28 19:17:07 +0300

    Rev midl

--- src/middlewares/validatorVideos.ts
+++ src/middlewares/validatorVideos.ts
@@ -9,7 +9,7 @@ export const videosValidator =
         body('availableResolutions').isArray(),
         body('minAgeRestriction').toInt().custom(val =>
         {
-            return val.toInt() > 1 && val.toInt() < 18;
+            return val.toInt() >= 1 && val.toInt() <= 18;
         }),
     ];
 
